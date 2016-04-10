c0d6a99a6fbea82b5744239aad3b4b5700345c2f
Bob Weigel <rweigel@gmu.edu>
2016-04-10 10:30:46 -0400

    misc

--- scope.js
+++ scope.js
@@ -1,4 +1,7 @@
-var a = 1; // Same result with or without var.  Good habit to keep it in case code is moved into a function.
+var a = 1; 
+// Same result with or without var.  Good habit to keep
+// it in case code is moved into a function.
+
 hello1();
 console.log(a); // 2
 
@@ -6,11 +9,14 @@ var a = 1; // Same result with or without var.
 hello2();
 console.log(a); // 1
 
-hello3();
-console.log(b)
+hello3(); // 99
+console.log(b) // 99
+
+call2x(2) // 2
+call2x() // undefined
 
 function hello1() {
-    a = 2; // Modifes external variable.
+    a = 2; // Modifes global variable.
     console.log(a);  // 2
 }
 
@@ -20,6 +26,13 @@ function hello2() {
 }
 
 function hello3() {
-    b = 99; // a is a global variable.
+    b = 99; // b is a global variable.
     console.log(b); // 99
+}
+
+function call2x(a) {
+	if (typeof(a) !== "undefined") {
+		var n = a
+	}
+	console.log(n)
 }
