a33183754165c501415978af322d298f9616f753
Bob Weigel <rweigel@gmu.edu>
2016-05-14 11:22:51 -0400

    New examples for node-java

--- node-java/java.js
+++ node-java/java.js
@@ -5,12 +5,41 @@ java.classpath.push('autoplot.jar');
 java.options.push('-Djava.awt.headless=true');
 java.options.push('-Xmx1024m');
 
-var strArray = java.newArray('java.lang.String',['-u','vap+jyds:https://raw.githubusercontent.com/autoplot/jyds/master/sscweb.jyds?timerange=2008-001','--format','dat']);
-var result = java.callStaticMethodSync("org.virbo.autoplot.AutoplotDataServer", "main", strArray);
-console.log(result)
+if (0) {
+	// Works
+	// java -jar bin/autoplot.jar "vap+cdaweb:ds=AC_H0_MFI&id=BGSEc&timerange=2014-08-27+through+2014-08-28"
+	var uri = 'vap+cdaweb:ds=AC_H0_MFI&id=BGSEc&timerange=2014-08-27+through+2014-08-28'
+	var strArray = java.newArray('java.lang.String',['--uri',uri,'--format','dat']);
+	var result = java.callStaticMethodSync("org.virbo.autoplot.AutoplotDataServer", "main", strArray);
+}
 
-var result = java.callStaticMethodSync("org.virbo.autoplot.AutoplotDataServer", "main", strArray);
-console.log(result)
+if (0) {
+	// Gives error. See command line display for error.
+	// java -Djava.awt.headless=true -cp autoplot.jar org.virbo.autoplot.AutoplotDataServer --uri "vap+jyds:file:/home/weigel/git/js-rsw/node-java/dumpMetadata.jyds" --format dat
+	var uri = 'vap+cdaweb:ds=AC_H0_MFI&id=BGSEc&timerange=2014-08-27+through+2014-08-28'
+	var strArray = java.newArray('java.lang.String',['--uri',uri,'--format','dat']);
+	var result = java.callStaticMethodSync("org.virbo.autoplot.AutoplotDataServer", "main", strArray);
+}
 
-//doService( String timeRange, String suri, String step, boolean stream, String format, final PrintStream out, boolean ascii, Set outEmpty, ProgressMonitor mon ) throws Exception {
-        
+if (0) {
+	// Works
+	//java -Djava.awt.headless=true -jar autoplot.jar org.virbo.autoplot.AutoplotUI --script dumpMetadata.jy	
+	var strArray = java.newArray('java.lang.String',['--script','dumpMetadata.jy']);
+	var result = java.callStaticMethodSync("org.virbo.autoplot.AutoplotUI", "main", strArray);
+	console.log(result) // null
+}
+
+if (0) {
+	// Error: only rank 1 and rank 2 data are supported
+	// java -Djava.awt.headless=true -jar autoplot.jar "vap+jyds:file:/home/weigel/git/js-rsw/node-java/getFillValue.jy"
+	var uri = 'vap+jyds:file:/home/weigel/git/js-rsw/node-java/getFillValue.jy'
+	var strArray = java.newArray('java.lang.String',['--uri',uri,'--format','dat']);
+	var result = java.callStaticMethodSync("org.virbo.autoplot.AutoplotDataServer", "main", strArray);
+	console.log(result)
+}
+
+if (0) {
+	var strArray = java.newArray('java.lang.String',['--script','getFillValue.jy']);
+	var result = java.callStaticMethodSync("org.virbo.autoplot.AutoplotUI", "main", strArray);
+	console.log(result)
+}
