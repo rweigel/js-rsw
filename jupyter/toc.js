2463e87b9c0e559cda9a087ab53d2ca09a6404df
Robert Weigel <rweigel@gmu.edu>
2018-09-18 12:11:40 -0400

    add toc.js for jypyter

---
+++ jupyter/toc.js
@@ -0,0 +1,7 @@
+$([IPython.events]).on('notebook_loaded.Notebook', function () {
+	$('h1').first().before('<h1 id="TOC">Table of Contents</h1><ol id="toclist"></ol>');
+	$('h1').each(function( index ) { if (index > 0) {$('#toclist').append('<li><a href="#'+$(this).attr('id')+'">'+$(this).html()+'</li>'); $(this).html(index + ". " + $(this).html()); }});
+	$('#toclist').click();
+	$('#toclist').clone().attr('id','toclist2').prependTo('#site').css('position','fixed').css('padding-left','1.5em').css('z-index',1)
+	$('#notebook-container').css('margin-right','1em')
+})
