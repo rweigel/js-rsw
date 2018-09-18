ffa214bdc1e10803b63f0799ad9c10d603b36d86
Robert Weigel <rweigel@gmu.edu>
2018-09-18 13:11:52 -0400

    add toc.js for jypyter

--- jupyter/toc.js
+++ jupyter/toc.js
@@ -1,8 +1,13 @@
-$([IPython.events]).on('notebook_loaded.Notebook', function () {
-	$('h1').first().before('<h1 id="TOC">Table of Contents</h1><ol id="toclist"></ol>');
-	$('h1').each(function( index ) { if (index > 0) {$('#toclist').append('<li><a href="#'+$(this).attr('id')+'">'+$(this).html()+'</li>'); $(this).html(index + ". " + $(this).html()); }});
-	$('#toclist').click();
-	$('#toclist').clone().attr('id','toclist2').prependTo('#site').css('position','fixed').css('padding-left','1.5em').css('z-index',1)
-	$('#notebook-container').css('margin-right','1em');
-	console.log('done')
-})
+%%javascript
+function updatetoc () {
+        console.log('Updating TOC');
+        $('.toc').remove();
+        //$.ajax({'url': "https://rawgit.com/rweigel/js-rsw/master/jupyter/toc.js"});
+        $('h1').first().before('<h1 class="toc" id="TOC">Table of Contents</h1><ol class="toc" id="toclist"></ol>');
+        $('h1').each(function( index ) { if (index > 0) {$('#toclist').append('<li><a href="#'+$(this).attr('id')+'">'+$(this).html()+'</li>'); $(this).html("<span class='toc'>" + index + ". </span>" + $(this).html()); }});
+        $('#toclist').click();
+        $('#toclist').clone().attr('id','toclist2').prependTo('#site').css('position','fixed').css('padding-left','1.5em').addClass('toc').css('z-index',1)
+        $('#notebook-container').css('margin-right','1em');
+}
+window.updatetoc = updatetoc;
+$([IPython.events]).on('notebook_loaded.Notebook', updatetoc);
